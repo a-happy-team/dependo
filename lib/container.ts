6f4a610d3e8ab61efa36dad4b3983996e2f6cf96
Vitor <lukassgm@gmail.com>
2024-03-01 16:07:35 -0500

    Register dependencies as an array

--- lib/container.ts
+++ lib/container.ts
@@ -1,97 +1,134 @@
-export type Token = Class | string
+export type Token = Class | string;
 
-export type Class = new (...args: any[]) => any
-export type ResolvedValue = string | number | boolean | InstanceType<Class>
+export type Class = new (...args: any[]) => any;
+export type ResolvedValue = string | number | boolean | InstanceType<Class>;
 
-export type ValueToBeResolved = Factory | Class | ResolvedValue
+export type ValueToBeResolved =
+	| Factory
+	| Class
+	| ResolvedValue
+	| (() => unknown);
 
-export type Factory = () => ResolvedValue
+export type Factory = () => ResolvedValue;
 
 export class Container {
-  private static instance: Container
-  private tokenToValue: Map<string, { singleton?: boolean; value: ValueToBeResolved }> = new Map();
-  private tokenToResolved: Map<string, ResolvedValue> = new Map();
-  
-  private constructor() {}
-  public static getInstance() {
-    if (!Container.instance) {
-      Container.instance = new Container()
-    }
-    return Container.instance
-  }
+	private static instance: Container;
+	private tokenToValue: Map<
+		string,
+		{ singleton?: boolean; value: Array<ValueToBeResolved> }
+	> = new Map();
+	private tokenToResolved: Map<string, ResolvedValue> = new Map();
 
+	private constructor() {}
+	public static getInstance() {
+		if (!Container.instance) {
+			Container.instance = new Container();
+		}
+		return Container.instance;
+	}
 
-  public resolve<T extends Token>(token: T): ResolvedValue {
-    let parsedToken: string = typeof token === 'string' ? token : token.name
+	public resolve<T extends Token>(token: T): ResolvedValue {
+		const parsedToken: string = typeof token === "string" ? token : token.name;
 
-    if (this.tokenToResolved.has(parsedToken)) {
-      return this.tokenToResolved.get(parsedToken) as ResolvedValue
-    }
+		if (this.tokenToResolved.has(parsedToken)) {
+			return this.tokenToResolved.get(parsedToken)[0] as ResolvedValue;
+		}
 
-    const storedValue = this.tokenToValue.get(parsedToken)
+		const storedValue = this.tokenToValue.get(parsedToken);
 
-    if (!storedValue) {
-      throw new Error(`No value found for token ${parsedToken}`)
-    }
+		if (!storedValue) {
+			throw new Error(`No value found for token ${parsedToken}`);
+		}
 
-    let instance: ResolvedValue = storedValue.value
+		let instance: ResolvedValue = storedValue.value[0];
 
-    if (this.isClass(storedValue.value)) {
-      instance = new storedValue.value
-    }
+		if (this.isClass(storedValue.value[0])) {
+			instance = new storedValue.value[0]();
+		}
 
-    if (this.isFactory(storedValue.value)) {
-      instance = storedValue.value()
-    }
+		if (this.isFactory(storedValue.value[0])) {
+			instance = storedValue.value[0]();
+		}
 
-    if (storedValue.singleton) {
-      this.tokenToResolved.set(parsedToken, instance)
-    }
+		if (storedValue.singleton) {
+			this.tokenToResolved.set(parsedToken, [instance]);
+		}
 
-    return instance
-  }
+		return instance;
+	}
 
-  public register<T extends Class>(clazz: T, value: T, singleton: boolean): void
-  public register<T extends ResolvedValue>(token: string, value: T, singleton: boolean): void
-  public register<T extends Factory>(token: string, value: T, singleton: boolean): void
-  public register<T extends Class | ResolvedValue | Factory>(token: string, value: T, singleton = false) {
-    if (this.isClass(token)) {
-      return this.registerClass(token, singleton)
-    }
-    
-    if (this.isFactory(value)) {
-      return this.registerFactory(token, value, singleton)
-    }
+	public register<T extends Class>(
+		clazz: T,
+		value: T,
+		singleton: boolean,
+	): void;
+	public register<T extends ResolvedValue>(
+		token: string,
+		value: T,
+		singleton: boolean,
+	): void;
+	public register<T extends Factory>(
+		token: string,
+		value: T,
+		singleton: boolean,
+	): void;
+	public register<T extends Class | ResolvedValue | Factory>(
+		token: string,
+		value: T,
+		singleton = false,
+	) {
+		if (this.isClass(token)) {
+			return this.registerClass(token, singleton);
+		}
 
-    return this.registerValue(token, value)
-  }
+		if (this.isFactory(value)) {
+			return this.registerFactory(token, value, singleton);
+		}
 
-  private registerClass(clazz: Class, singleton = false) {
-    if (this.tokenToValue.has(clazz.name)) {
-      throw new Error(`Class with name ${clazz.name} is already registered`)
-    }
+		return this.registerValue(token, value);
+	}
 
-    this.tokenToValue.set(clazz.name, { singleton, value: clazz })
-  }
+	private registerClass(clazz: Class, singleton = false) {
+		const register = this.tokenToValue.get(clazz.name);
 
-  private registerValue<T>(name: string, value: T) {
-    this.tokenToResolved.set(name, value)
-  }
+		const values = register?.value || [];
 
-  private registerFactory<T>(name: string, factory: () => T, singleton = false) {
-    this.tokenToResolved.set(name, factory())
-  }
+		this.tokenToValue.set(clazz.name, { singleton, value: [clazz, ...values] });
+	}
 
-  dispose() {
-    this.tokenToResolved.clear()
-    this.tokenToValue.clear()
-  }
+	private registerValue<T>(name: string, value: T) {
+		const register = this.tokenToResolved.get(name);
 
-  private isClass(value: any): value is Class {
-    return typeof value === 'function' && Object.getOwnPropertyNames(value).includes('prototype')
-  }
+		const values = register?.value || [];
 
-  private isFactory(value: any): value is Factory {
-    return typeof value === 'function' && !this.isClass(value)
-  }
+		this.tokenToResolved.set(name, [value, ...values]);
+	}
+
+	private registerFactory<T>(
+		name: string,
+		factory: () => T,
+		singleton = false,
+	) {
+		const register = this.tokenToValue.get(name);
+
+		const values = register?.value || [];
+
+		this.tokenToValue.set(name, { singleton, value: [factory, ...values] });
+	}
+
+	dispose() {
+		this.tokenToResolved.clear();
+		this.tokenToValue.clear();
+	}
+
+	private isClass(value: any): value is Class {
+		return (
+			typeof value === "function" &&
+			Object.getOwnPropertyNames(value).includes("prototype")
+		);
+	}
+
+	private isFactory(value: any): value is Factory {
+		return typeof value === "function" && !this.isClass(value);
+	}
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,173 +1,173 @@
-import assert from 'node:assert'
-import test from 'node:test'
-import { container, inject, injectable } from '../lib'
-import { Container } from '../lib/container'
+import assert from "node:assert";
+import test from "node:test";
+import { container, inject, injectable } from "../lib";
+import { Container } from "../lib/container";
 
-test('should instantiate container correctly', (t) => {
-  assert.strictEqual(container instanceof Container, true)  
-})
+test("should instantiate container correctly", (t) => {
+	assert.strictEqual(container instanceof Container, true);
+});
 
-test('should register class correctly', (t) => {
-  class TestClass0 {}
-  container.register(TestClass0, TestClass0, false)
-  console.log(container['tokenToValue'])
-  assert.strictEqual(container['tokenToValue'].has('TestClass0'), true)
-})
+test("should register class correctly", (t) => {
+	class TestClass0 {}
+	container.register(TestClass0, TestClass0, false);
+	console.log(container["tokenToValue"]);
+	assert.strictEqual(container["tokenToValue"].has("TestClass0"), true);
+});
 
-test('should register class using decorator correctly', (t) => {
-  @injectable()
-  class TestClass00 {}
-  assert.strictEqual(container['tokenToValue'].has('TestClass00'), true)
-})
+test("should register class using decorator correctly", (t) => {
+	@injectable()
+	class TestClass00 {}
+	assert.strictEqual(container["tokenToValue"].has("TestClass00"), true);
+});
 
-test('should register a class as a singleton correctly', (t) => {
-  @injectable({ singleton: true})
-  class TestClass1 {}
-  assert.strictEqual(container['tokenToValue'].get('TestClass1')?.singleton
-    , true)
-})
+test("should register a class as a singleton correctly", (t) => {
+	@injectable({ singleton: true })
+	class TestClass1 {}
+	assert.strictEqual(
+		container["tokenToValue"].get("TestClass1")?.singleton,
+		true,
+	);
+});
 
-test('should resolve class correctly', (t) => {
-  @injectable()
-  class TestClass2 {}
-  const instance = container.resolve(TestClass2)
-  assert.strictEqual(instance instanceof TestClass2, true)
-})
+test("should resolve class correctly", (t) => {
+	@injectable()
+	class TestClass2 {}
+	const instance = container.resolve(TestClass2);
+	assert.strictEqual(instance instanceof TestClass2, true);
+});
 
-test('should resolve class as a singleton correctly', (t) => {
-  @injectable({ singleton: true })
-  class TestClass3 {}
-  const instance = container.resolve(TestClass3)
-  const instance2 = container.resolve(TestClass3)
-  assert.strictEqual(instance, instance2)
-})
+test("should resolve class as a singleton correctly", (t) => {
+	@injectable({ singleton: true })
+	class TestClass3 {}
+	const instance = container.resolve(TestClass3);
+	const instance2 = container.resolve(TestClass3);
+	assert.strictEqual(instance, instance2);
+});
 
-test('should throw error when resolving unregistered class', (t) => {
-  class TestClass4 {}
-  assert.throws(() => container.resolve(TestClass4))
-})
+test("should throw error when resolving unregistered class", (t) => {
+	class TestClass4 {}
+	assert.throws(() => container.resolve(TestClass4));
+});
 
-test('should throw error when registering class with same name', (t) => {
-  class TestClass5 {}
-  container.register(TestClass5, TestClass5, false)
-  assert.throws(() => container.register(TestClass5,TestClass5, false))
-})
+test("should dispose container correctly", (t) => {
+	class TestClass6 {}
+	container.register(TestClass6, TestClass6, false);
+	container.dispose();
+	assert.strictEqual(container["tokenToValue"].size, 0);
+	assert.strictEqual(container["tokenToResolved"].size, 0);
+});
 
-test('should dispose container correctly', (t) => {
-  class TestClass6 {}
-  container.register(TestClass6, TestClass6, false)
-  container.dispose()
-  assert.strictEqual(container['tokenToValue'].size, 0)
-  assert.strictEqual(container['tokenToResolved'].size, 0)
-})
+test("should resolve class with dependencies correctly", (t) => {
+	@injectable()
+	class TestClass7 {}
+	@injectable()
+	class TestClass8 {
+		@inject(TestClass7) testClass7: TestClass7;
+	}
+	const instance = container.resolve(TestClass8);
+	assert.strictEqual(instance.testClass7 instanceof TestClass7, true);
+});
 
-test('should resolve class with dependencies correctly', (t) => {
-  @injectable()
-  class TestClass7 {}
-  @injectable()
-  class TestClass8 {
-    @inject(TestClass7) testClass7: TestClass7
-  }
-  const instance = container.resolve(TestClass8)
-  assert.strictEqual(instance.testClass7 instanceof TestClass7, true)
-})
+test("should resolve class with dependencies as singletons correctly", (t) => {
+	@injectable({ singleton: true })
+	class TestClass9 {}
+	@injectable()
+	class TestClass10 {
+		@inject(TestClass9) testClass9: TestClass9;
+	}
+	const instance = container.resolve(TestClass10);
+	const instance2 = container.resolve(TestClass10);
+	assert.strictEqual(instance.testClass9, instance2.testClass9);
+});
 
-test('should resolve class with dependencies as singletons correctly', (t) => {
-  @injectable({ singleton: true })
-  class TestClass9 {}
-  @injectable()
-  class TestClass10 {
-    @inject(TestClass9) testClass9: TestClass9
-  }
-  const instance = container.resolve(TestClass10)
-  const instance2 = container.resolve(TestClass10)
-  assert.strictEqual(instance.testClass9, instance2.testClass9)
-})
+test("should resolve class with lots of dependencies correctly", (t) => {
+	@injectable({ singleton: true })
+	class TestClass11 {}
+	@injectable()
+	class TestClass12 {
+		@inject(TestClass11) testClass11: TestClass11;
+	}
+	@injectable()
+	class TestClass13 {
+		@inject(TestClass12) testClass12: TestClass12;
+	}
+	@injectable()
+	class TestClass14 {
+		@inject(TestClass13) testClass13: TestClass13;
+	}
+	@injectable()
+	class TestClass15 {
+		@inject(TestClass14) testClass14: TestClass14;
+	}
 
-test('should resolve class with lots of dependencies correctly', (t) => {
-  @injectable({ singleton: true })
-  class TestClass11 {}
-  @injectable()
-  class TestClass12 {
-    @inject(TestClass11) testClass11: TestClass11
-  }
-  @injectable()
-  class TestClass13 {
-    @inject(TestClass12) testClass12: TestClass12
-  }
-  @injectable()
-  class TestClass14 {
-    @inject(TestClass13) testClass13: TestClass13
-  }
-  @injectable()
-  class TestClass15 {
-    @inject(TestClass14) testClass14: TestClass14
-  }
+	@injectable()
+	class TestClass16 {
+		@inject(TestClass15) testClass15: TestClass15;
+		@inject(TestClass14) testClass14: TestClass14;
+		@inject(TestClass13) testClass13: TestClass13;
+		@inject(TestClass12) testClass12: TestClass12;
+		@inject(TestClass11) testClass11: TestClass11;
+	}
 
-  @injectable()
-  class TestClass16 {
-    @inject(TestClass15) testClass15: TestClass15
-    @inject(TestClass14) testClass14: TestClass14
-    @inject(TestClass13) testClass13: TestClass13
-    @inject(TestClass12) testClass12: TestClass12
-    @inject(TestClass11) testClass11: TestClass11
-  }
+	const instance = container.resolve(TestClass15);
+	assert.strictEqual(
+		instance.testClass14.testClass13.testClass12.testClass11 instanceof
+			TestClass11,
+		true,
+	);
 
-  const instance = container.resolve(TestClass15)
-  assert.strictEqual(instance.testClass14.testClass13.testClass12.testClass11 instanceof TestClass11, true)
-  
-  const instance2 = container.resolve(TestClass16)
+	const instance2 = container.resolve(TestClass16);
 
-  assert.strictEqual(instance2 instanceof TestClass16, true)
-})
+	assert.strictEqual(instance2 instanceof TestClass16, true);
+});
 
-test('should register factory correctly', () => {
-  const factory = () => 'test'
-  container.register('test', factory, false)
-  assert.strictEqual(container['tokenToResolved'].has('test'), true)
-})
+test("should register factory correctly", () => {
+	const factory = () => "test";
+	container.register("test", factory, false);
+	assert.strictEqual(container["tokenToValue"].has("test"), true);
+});
 
-test('should resolve factory correctly', () => {
-  const factory = () => 'test'
-  container.register('test', factory, false)
-  const instance = container.resolve('test')
-  assert.strictEqual(instance, 'test')
-})
+test("should resolve factory correctly", () => {
+	const factory = () => "test";
+	container.register("test", factory, false);
+	const instance = container.resolve("test");
+	assert.strictEqual(instance, "test");
+});
 
-test('should resolve factory as a singleton correctly', () => {
-  var counter = 1
+test("should resolve factory as a singleton correctly", () => {
+	var counter = 1;
 
-  const factory = () => counter++
-  container.register('test', factory, true)
-  const instance = container.resolve('test')
-  const instance2 = container.resolve('test')
-  assert.strictEqual(instance, instance2)
-})
+	const factory = () => counter++;
+	container.register("test", factory, true);
+	const instance = container.resolve("test");
+	const instance2 = container.resolve("test");
+	assert.strictEqual(instance, instance2);
+});
 
-test('should register value correctly', () => {
-  container.register('test1', 'test1', false)
-  assert.strictEqual(container['tokenToResolved'].has('test1'), true)
-})
+test("should register value correctly", () => {
+	container.register("test1", "test1", false);
+	assert.strictEqual(container["tokenToResolved"].has("test1"), true);
+});
 
-test('should resolve value correctly', () => {
-  container.register('test2', 'test2', false)
-  const instance = container.resolve('test2')
-  assert.strictEqual(instance, 'test2')
-})
+test("should resolve value correctly", () => {
+	container.register("test2", "test2", false);
+	const instance = container.resolve("test2");
+	assert.strictEqual(instance, "test2");
+});
 
-test('should resolve value as a singleton correctly', () => {
-  container.register('test3', 'test3', true)
-  const instance = container.resolve('test3')
-  const instance2 = container.resolve('test3')
-  assert.strictEqual(instance, instance2)
-})
+test("should resolve value as a singleton correctly", () => {
+	container.register("test3", "test3", true);
+	const instance = container.resolve("test3");
+	const instance2 = container.resolve("test3");
+	assert.strictEqual(instance, instance2);
+});
 
-test('should be able to inject using string token', () => {
-  container.register('test3', 'test3', false)
-  @injectable()
-  class TestClass17 {
-    @inject('test3') test3: string
-  }
-  const instance = container.resolve(TestClass17)
-  assert.strictEqual(instance.test3, 'test3')
-})
+test("should be able to inject using string token", () => {
+	container.register("test3", "test3", false);
+	@injectable()
+	class TestClass17 {
+		@inject("test3") test3: string;
+	}
+	const instance = container.resolve(TestClass17);
+	assert.strictEqual(instance.test3, "test3");
+});
